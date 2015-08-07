28f2684a730b28914cebe8447926e08fa2832c3a
Hiram J. Pérez <worg@linuxmail.org>
2015-08-07 13:23:22 -0500

    use unicode

--- gitlab2tele/__init__.py
+++ gitlab2tele/__init__.py
@@ -22,9 +22,9 @@ class TeleSender():
 
     def __parse_push_event(self, json_obj):
         d = json_obj;
-        ret = '%s pushed %d commits to %s<%s> \n' % (d['user_name'], d['total_commits_count'], d['repository']['name'], d['ref'].replace('refs/heads/',''))
+        ret = u'%s pushed %d commits to %s<%s> \n' % (d['user_name'], d['total_commits_count'], d['repository']['name'], d['ref'].replace('refs/heads/',''))
         for c in d['commits']:
-            ret += '%s: %s - %s \n' % (c['id'][:7], c['message'][:80].rstrip(), c['author']['name']) #print commit summary
+            ret += u'%s: %s - %s \n' % (c['id'][:7], c['message'][:80].rstrip(), c['author']['name']) #print commit summary
         return ret
 
     def __revice_merge_request(self, json_obj):
